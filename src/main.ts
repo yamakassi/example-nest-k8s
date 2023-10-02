a378220248e3f3e446c1a5ec1ee03df8effcacdf
yamakassi <yamakassi451@gmail.com>
2023-10-03 00:03:57 +0400

    main

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AppService } from './app.service';
-
 
 async function bootstrap() {
-  const isCronJob = process.env.CRONJOB === 'CRONJOB';
   const app = await NestFactory.create(AppModule);
-
-  await app.listen(3000, () => {
-    if (isCronJob) {
-      const statusJob = app.get(AppService);
-      statusJob.subscribeToShutdown(async (code) => {
-        await app.close();
-        console.log('Process exiting...');
-        process.exit(code || 0);
-      });
-      statusJob.foo();
-    }
-  });
+  await app.listen(3000);
 }
 bootstrap();
